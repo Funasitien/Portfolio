717021c9d354720be9bf5533138c59ec06c5b9ab
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2025-03-23 10:44:16 +0100

    🐛 Push test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  integrations: [icon(), mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://f.dreamclouds.fr",
-  base: '/Portfolio',
-  integrations: [tailwind(), icon(), mdx()]
+  base: '/Portfolio'
 });
